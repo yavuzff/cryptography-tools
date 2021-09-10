75d55f35484efbb9eb27fc1ebae76fd0abc154bb
yavuzff <75276351+yavuzff@users.noreply.github.com>
2021-09-10 18:48:09 +0100

    Caesar

--- caesar.py
+++ caesar.py
@@ -1,3 +1,5 @@
+
+
 def caesar (encrypt,shift,text, alphabet="abcdefghijklmnopqrstuvwxyz"): #key is a number
     shift = shift % len(alphabet) #lengths greater than the length of alphabet are handled
 
@@ -28,3 +30,25 @@ def caesarBrute(text, alphabet="abcdefghijklmnopqrstuvwxyz"): #goes through all 
         print ('Key ' + str(i+1)+ ': ' + possibilities[i])
 
     return possibilities
+
+def alphabetSub(encrypt,key,text,orig='abcdefghijklmnopqrstuvwxyz'):
+    #the orig optional parameter will allow to specify extra symbols such as punctation
+
+    key = key.upper()  #text and keys converted to upper case
+    orig = orig.upper()
+    text = text.upper()
+    
+    if encrypt: #dictionary if encrypting
+        mappings = dict(zip(list(orig),list(key)))
+    else:       #dictionary if decrypting
+        mappings = dict(zip(list(key),list(orig)))
+
+    newText = ''
+    for i in text:              #for each character in the text
+        if i not in mappings:   #if it is not part of the key
+            newText += i        #it is not encrypted
+        else:
+            newText += mappings[i]
+            #the encrypted version from the hash table retrieved
+
+    return newText
