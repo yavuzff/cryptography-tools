e15736e72daabaf05cc9b56b7e41f87094ff414b
yavuzff <75276351+yavuzff@users.noreply.github.com>
2021-11-18 20:22:03 +0000

    update hill cipher

--- hillcipher.py
+++ hillcipher.py
@@ -86,7 +86,7 @@ def cipher(encrypt, keymatrix, text, alphabet):
     return new
 
 #returns modular inverse of a number
-def muodularInverse(a,n): #n is length of alphabet
+def modularInverse(a,n): #n is length of alphabet
     for i in range (1,n+1):
         value = (((i*n)+1)/a)
         if value.is_integer():
